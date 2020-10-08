fd5c5027e7af9bfb8868544b43096a3adb21b729
selvaraj <selvabe99@gmail.com>
2020-10-08 22:44:30 +0530

    Getting the data out but the not in sequence

--- src/fifo/fifo.v
+++ src/fifo/fifo.v
@@ -1,34 +1,47 @@
 // test project for revising the verilog
 
-module  fifo ( out_d, out_clk , inp_clk, inp_d, reset);
+module  fifo ( out_d, out_clk, read_flg, inp_clk, inp_d, write_flg, reset);
 
 input wire reset;
 input wire[7:0] inp_d;
 input wire inp_clk;
 input wire out_clk;
+input wire read_flg;
+input wire write_flg;
 output reg[7:0] out_d;
-  
+
 reg [32:0] buffer[8];
 
 reg [7:0] inp_addr;
 reg [7:0] out_addr;
 
+integer j;
+
 always @(posedge reset)
 begin
     inp_addr <= 0;
     out_addr <= 0;
+    for (j=0; j < 8 ; j=j+1) begin
+        buffer[j] <= 8'b0; //reset array
+    end
 end
 
 always @(posedge inp_clk)
 begin
-    buffer[inp_addr] <= inp_d;
-    inp_addr <= inp_addr + 1;
+    if (write_flg)
+    begin
+        buffer[inp_addr] <= inp_d;
+        inp_addr <= inp_addr + 1;
+    end
 end
 
-always @(posedge out_clk)
+always @(posedge out_clk) 
 begin
-    out_d <= buffer[out_addr];
-    out_addr <= out_addr + 1;
+    if (read_flg)
+    begin
+        out_d <= buffer[out_addr];
+        out_addr <= out_addr + 1;
+    end
 end
 
 endmodule
--- src/fifo/tb_fifo.v
+++ src/fifo/tb_fifo.v
@@ -6,15 +6,20 @@ reg [7:0] inp_d;
 reg inp_clk;
 reg out_clk;
 wire [7:0] out_d;
+reg read_flg;
+reg write_flg;
 
 // instance of the module 
-fifo dut( .out_d(out_d), .out_clk(inp_clk), .inp_clk(inp_clk), .inp_d(inp_d), .reset(reset));
+fifo dut( .out_d(out_d), .out_clk(inp_clk), .read_flg(read_flg), 
+    .inp_clk(inp_clk), .inp_d(inp_d), .write_flg(write_flg), .reset(reset));
 
-// provide clock 
+// reset the vriable & provide clock 
 initial 
 begin 
 inp_clk <= 0;
 out_clk <= 0;
+read_flg <=0;
+write_flg <=0;
 forever #10 inp_clk =~inp_clk;
 forever #10 out_clk =~out_clk;
 end
@@ -24,13 +29,24 @@ initial
 begin 
 #50;
 reset <= 0;
-#50
+#25
+reset <= 1;     //reset the module
+#25
+write_flg <= 1; //write start
 inp_d <= 0;
 #10;
-inp_d <= 8;
+inp_d <= 1;
 #10;
-inp_d <= 0;
-#50;
+inp_d <= 2;
+#10;
+inp_d <= 3;
+#10;
+write_flg <= 0; // write complete
+#10
+read_flg <= 1; // read start 
+#70
+read_flg <= 0; // read start 
+#20
 $finish;
 end
 
