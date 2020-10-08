6b1369d8c47789c70a13e8c9c2374ef53ef3524e
selvaraj <selvabe99@gmail.com>
2020-10-08 23:07:41 +0530

    Fifo working

--- src/fifo/tb_fifo.v
+++ src/fifo/tb_fifo.v
@@ -8,6 +8,7 @@ reg out_clk;
 wire [7:0] out_d;
 reg read_flg;
 reg write_flg;
+integer j;
 
 // instance of the module 
 fifo dut( .out_d(out_d), .out_clk(inp_clk), .read_flg(read_flg), 
@@ -27,25 +28,24 @@ end
 // provide input to the module 
 initial 
 begin 
-#50;
+#10;
 reset <= 0;
-#25
+#10
 reset <= 1;     //reset the module
-#25
+#10
 write_flg <= 1; //write start
-inp_d <= 0;
 #10;
-inp_d <= 1;
-#10;
-inp_d <= 2;
-#10;
-inp_d <= 3;
+for (j=0; j < 4 ; j=j+1) 
+begin
+    inp_d <= j; 
+    #20;
+end
 #10;
 write_flg <= 0; // write complete
 #10
 read_flg <= 1; // read start 
-#70
-read_flg <= 0; // read start 
+#100
+read_flg <= 0; // read end
 #20
 $finish;
 end
@@ -53,13 +53,13 @@ end
 // dump the output 
 initial
 begin
-	$dumpfile("test.vcd");
-	$dumpvars(0,inp_d);
-	$dumpvars(0,inp_clk);
-	$dumpvars(0,out_d);
-	$dumpvars(0,out_clk);
-	$dumpvars(0,reset);
-	$dumpvars(0,dut);
+    $dumpfile("test.vcd");
+    $dumpvars(0,inp_d);
+    $dumpvars(0,inp_clk);
+    $dumpvars(0,out_d);
+    $dumpvars(0,out_clk);
+    $dumpvars(0,reset);
+    $dumpvars(0,dut);
 end
 
 endmodule
