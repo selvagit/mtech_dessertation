9df1707c99631a9f2305191a34530932fcef2696
selvaraj <selvabe99@gmail.com>
2020-10-25 02:29:54 +0530

    The interface changed to add three memory port , now the test bench is broken

--- src/ecpri/rx_tx/ecpri_rx.v
+++ src/ecpri/rx_tx/ecpri_rx.v
@@ -1,26 +1,59 @@
-// test project for revising the verilog
+//-----------------------------------------------------
+// Design Name : ecpri
+// File Name   : ecpri_rx.v
+// Function    : module to handle epcri rx packet 
+// Coder       : selvaraj 
+//-----------------------------------------------------
+module  ecpri_rx ( send_write_resp, send_read_resp, resp_payload_len, 
+                    addr_0, data_0, we_0, oe_0, //info_to_tx, 
+                    addr_1, data_1, we_1, oe_1, 
+                    addr_2, data_2, we_2, oe_2, //data_to_mem,   
+                    clk, inp_data_fifo, recv_pkt, reset);
 
-module  ecpri_rx ( tx_payload_len, info_to_tx, data_to_mem, send_write_resp, send_read_resp,
-                    clk, rx_buff, read_flg, reset);
-input wire reset;
-input wire[7:0] rx_buff;
-input wire clk;
-input wire read_flg;
+parameter DATA_WIDTH = 8 ;
+parameter ADDR_WIDTH = 16 ;
+
+//--------------Input Ports----------------------- 
+input reset;
+input [DATA_WIDTH-1:0] inp_data_fifo;
+input clk;
+input recv_pkt;
+
+//--------------Out Ports----------------------- 
 output reg send_write_resp;
 output reg send_read_resp;
-output reg [7:0] info_to_tx;
-output reg [7:0] tx_payload_len;
-output reg [7:0] data_to_mem;
+output reg [DATA_WIDTH-1:0] resp_payload_len;
 
-reg [7:0] inp_addr;
-reg [7:0] payload_len;
+// port to copy the ethernet packet 
+output reg [ADDR_WIDTH-1:0] addr_0;
+output reg [DATA_WIDTH-1:0] data_0;
+output we_0;
+output oe_0;
+
+// port to transfer  
+output reg [ADDR_WIDTH-1:0] addr_1;
+output reg [DATA_WIDTH-1:0] data_1;
+output we_1;
+output oe_1;
+
+// port to tranfer the ecpri paload 
+output reg [ADDR_WIDTH-1:0] addr_2;
+output reg [DATA_WIDTH-1:0] data_2;
+output we_2;
+output oe_2;
+
+//--------------Internal variables---------------- 
+reg [ADDR_WIDTH-1:0] inp_addr;
+reg [ADDR_WIDTH-1:0] dst_addr;     
+
+reg [ADDR_WIDTH-1:0] payload_len;
+reg [DATA_WIDTH-1:0] inp_d;     
 
 reg [7:0] state;
 reg [7:0] nextstate;     
-reg [7:0] inp_d;     
-reg [7:0] dst_addr;     
 reg [7:0] info_to_rx;
 
+//-------------- Ethernet header offset and length ----------------------- 
 parameter  eth_hdr_offset = 0;
 parameter  eth_hdr_len = 14;
 parameter  ip_hdr_offset = 14;
@@ -29,6 +62,7 @@ parameter  udp_hdr_start = 34;
 parameter  udp_hdr_len = 8;
 parameter  vlan_offset = 0;
 
+//-------------- ecpri state machine ------------------------------ 
 parameter  reset_rx = 0;
 parameter  cpri_hdr = 1;
 parameter  cpri_type = 2;
@@ -42,6 +76,7 @@ parameter  write_payload = 10;
 parameter  write_to_mem = 11;
 parameter  raise_tx_resp = 12;
 
+//-------------- ecpri state machine ------------------------------ 
 // always block to update state 
 always @(posedge clk or posedge reset) begin   
     if (reset) begin      
@@ -57,6 +92,8 @@ always @(posedge clk or posedge reset) begin
 end
 
 // always block for doing the state activity
+// increment the address in every state to get the data 
+// for every cycle 
 always @(state)  begin
     case (state)
         reset_rx :begin end
@@ -76,92 +113,92 @@ always @(state)  begin
 end	
 
 // always block to compute nextstate 
-always @(clk or state or rx_buff or read_flg) begin    
+always @(clk or state or inp_data_fifo or recv_pkt) begin    
     nextstate = reset_rx;     
     case (state)
         reset_rx: begin
-            if (read_flg) begin 
+            if (recv_pkt) begin 
                 nextstate = cpri_hdr;
-                inp_addr = rx_buff + udp_hdr_start + udp_hdr_len ;
+                inp_addr = inp_data_fifo + udp_hdr_start + udp_hdr_len ;
             end
         end
 
         cpri_hdr: begin  // check the ecpri write flag is set 
-        if (inp_d == 8'h10) begin         
-            nextstate = cpri_type;
+            if (inp_d == 8'h10) begin         
+                nextstate = cpri_type;
+            end
         end
-    end
 
-    cpri_type: begin
-        if (inp_d == 8'h10) begin        // check the write flag
-            nextstate = write_id;
+        cpri_type: begin
+            if (inp_d == 8'h10) begin        // check the write flag
+                nextstate = write_id;
+            end
+            if (inp_d == 8'h00) begin        // check the read flag
+                nextstate = read_id;
+            end
         end
-        if (inp_d == 8'h00) begin        // check the read flag
-            nextstate = read_id;
+
+        read_id:  begin 
+            if (inp_d == 8'h00) begin
+                nextstate = read_mem;
+            end
         end
-    end
 
-    read_id:  begin 
-    if (inp_d == 8'h00) begin
-        nextstate = read_mem;
-    end
-end
+        read_mem : begin
+            nextstate = read_payload;
+            dst_addr = inp_d;
+            resp_payload_len = payload_len;
+        end
 
-read_mem : begin
-    nextstate = read_payload;
-    dst_addr = inp_d;
-    tx_payload_len = payload_len;
-end
+        read_payload : begin
+            if (inp_addr == 8'h12) begin
+                payload_len  = inp_d;
+                nextstate = raise_rx_resp;
+            end
+        end
 
-read_payload : begin
-    if (inp_addr == 8'h12) begin
-        payload_len  = inp_d;
-        nextstate = raise_rx_resp;
-    end
-end
+        raise_rx_resp : begin
+            send_read_resp = 1;
+            nextstate = reset_rx;
+        end
 
-raise_rx_resp : begin
-    send_read_resp = 1;
-    nextstate = reset_rx;
-end
+        write_id : begin
+        end
 
-write_id : begin
-end
+        write_mem : begin
+            // get the dst_address memory address 
+            if (inp_addr == 8'h13) begin
+                dst_addr = inp_d;
+                resp_payload_len = payload_len;
+            end
+        end
 
-write_mem : begin
-    // get the dst_address memory address 
-    if (inp_addr == 8'h13) begin
-        dst_addr = inp_d;
-        tx_payload_len = payload_len;
-    end
-end
+        write_payload : begin
+            // get the length of the payload 
+            if (inp_addr == 8'h12) begin
+                payload_len  = inp_d;
+            end
+        end
 
-write_payload : begin
-    // get the length of the payload 
-    if (inp_addr == 8'h12) begin
-        payload_len  = inp_d;
-    end
-end
+        write_to_mem : begin
+            // copy the payload 
+            if ( payload_len > 8'h0) begin
+                dst_addr = inp_d;
+                payload_len = payload_len - 1;
+                dst_addr = dst_addr + 1;
+                data_2 = 'h1;
+                data_0 = 'h1;
+            end
+        end
 
-write_to_mem : begin
-    // copy the payload 
-    if ( payload_len > 8'h0) begin
-        dst_addr = inp_d;
-        payload_len = payload_len - 1;
-        dst_addr = dst_addr + 1;
-        data_to_mem = 'h1;
-        info_to_tx = 'h1;
-    end
-end
+        raise_tx_resp : begin
+            send_write_resp <= 1;
+            nextstate = reset_rx;
+        end
 
-raise_tx_resp : begin
-    send_write_resp <= 1;
-    nextstate = reset_rx;
-end
-
-default : begin
-end
-endcase
+        default : begin
+        end
+    endcase
 end
 
 endmodule //ecpri_rx
--- src/ecpri/rx_tx/ecpri_tb.v
+++ src/ecpri/rx_tx/ecpri_tb.v
@@ -1,38 +1,53 @@
 // create test bench
 module tb_ecpri();
 
-reg reset;
-reg[7:0] rx_buff;
-reg clk;
-reg read_flg;
-wire send_write_resp;
-wire send_read_resp;
-wire [7:0] info_to_tx;
-wire [7:0] tx_payload_len;
-wire [7:0] data_to_mem;
+parameter DATA_WIDTH = 8 ;
+parameter ADDR_WIDTH = 16 ;
+
+output send_write_resp, send_read_resp; 
+output [DATA_WIDTH-1:0] resp_payload_len; 
+
+output [ADDR_WIDTH-1:0] addr_0;
+output [DATA_WIDTH-1:0] data_0; 
+output we_0, oe_0; //info_to_tx, 
+
+output [ADDR_WIDTH-1:0] addr_1;
+output [DATA_WIDTH-1:0] data_1; 
+output we_1, oe_1; 
+
+output [ADDR_WIDTH-1:0] addr_2; 
+output [DATA_WIDTH-1:0] data_2; 
+output we_2, oe_2; //data_to_mem,   
+
+input clk; 
+input [DATA_WIDTH-1:0] inp_data_fifo; 
+input reg recv_pkt, reset;
+
 integer j;
 
-ecpri_rx dut_ecpri_rx( tx_payload_len, info_to_tx, data_to_mem, send_write_resp, send_read_resp,
-                    clk, rx_buff, read_flg, reset);
-
+ecpri_rx dut_ecpri_rx(
+    send_write_resp, send_read_resp, resp_payload_len, 
+    addr_0, data_0, we_0, oe_0, //info_to_tx, 
+    addr_1, data_1, we_1, oe_1, 
+    addr_2, data_2, we_2, oe_2, //data_to_mem,   
+    clk, inp_data_fifo, recv_pkt, reset
+);
+    
 // reset the vriable & provide clock 
-initial 
-begin 
-forever #10 clk =~clk;
+initial begin 
+    forever #10 clk =~clk;
 end
 
 // provide input to the module 
-initial 
-begin 
-#10;
-reset <= 0;
-#20
-$finish;
+initial begin 
+    #10;
+    reset <= 0;
+    #20
+    $finish;
 end
 
 // dump the output 
-initial
-begin
+initial begin
     $dumpfile("ecpri.vcd");
     $dumpvars(0,dut_ecpri_rx);
 end
