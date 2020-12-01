7b9ba9371ac6da7e5c3a35dfc9dc2de9ff47afa7
selvaraj <selvabe99@gmail.com>
2020-12-01 21:59:06 +0530

    Add the vivado project

---
+++ src/integration/ip_repo/ecpri_2.0/drivers/ecpri_v1_0/data/ecpri.tcl
@@ -0,0 +1,5 @@
+
+
+proc generate {drv_handle} {
+	xdefine_include_file $drv_handle "xparameters.h" "ecpri" "NUM_INSTANCES" "DEVICE_ID" 
+}
---
+++ src/integration/ip_repo/ecpri_2.0/drivers/ecpri_v1_0/src/ecpri.h
@@ -0,0 +1,74 @@
+
+#ifndef ECPRI_H
+#define ECPRI_H
+
+
+/****************** Include Files ********************/
+#include "xil_types.h"
+#include "xstatus.h"
+
+
+/**************************** Type Definitions *****************************/
+/**
+ *
+ * Write a value to a ECPRI register. A 32 bit write is performed.
+ * If the component is implemented in a smaller width, only the least
+ * significant data is written.
+ *
+ * @param   BaseAddress is the base address of the ECPRIdevice.
+ * @param   RegOffset is the register offset from the base to write to.
+ * @param   Data is the data written to the register.
+ *
+ * @return  None.
+ *
+ * @note
+ * C-style signature:
+ * 	void ECPRI_mWriteReg(u32 BaseAddress, unsigned RegOffset, u32 Data)
+ *
+ */
+#define ECPRI_mWriteReg(BaseAddress, RegOffset, Data) \
+  	Xil_Out32((BaseAddress) + (RegOffset), (u32)(Data))
+
+/**
+ *
+ * Read a value from a ECPRI register. A 32 bit read is performed.
+ * If the component is implemented in a smaller width, only the least
+ * significant data is read from the register. The most significant data
+ * will be read as 0.
+ *
+ * @param   BaseAddress is the base address of the ECPRI device.
+ * @param   RegOffset is the register offset from the base to write to.
+ *
+ * @return  Data is the data from the register.
+ *
+ * @note
+ * C-style signature:
+ * 	u32 ECPRI_mReadReg(u32 BaseAddress, unsigned RegOffset)
+ *
+ */
+#define ECPRI_mReadReg(BaseAddress, RegOffset) \
+    Xil_In32((BaseAddress) + (RegOffset))
+
+/************************** Function Prototypes ****************************/
+/**
+ *
+ * Run a self-test on the driver/device. Note this may be a destructive test if
+ * resets of the device are performed.
+ *
+ * If the hardware system is not built correctly, this function may never
+ * return to the caller.
+ *
+ * @param   baseaddr_p is the base address of the ECPRI instance to be worked on.
+ *
+ * @return
+ *
+ *    - XST_SUCCESS   if all self-test code passed
+ *    - XST_FAILURE   if any self-test code failed
+ *
+ * @note    Caching must be turned off for this function to work.
+ * @note    Self test may fail if data memory and device are not on the same bus.
+ *
+ */
+XStatus ECPRI_Reg_SelfTest(void * baseaddr_p);
+
+#endif // ECPRI_H
